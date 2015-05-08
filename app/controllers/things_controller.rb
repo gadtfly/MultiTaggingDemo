a96dc9d587e24209c555dd832bdb9fd5e547cf17
Adam Louis <adamlouis@gmail.com>
2015-05-08 10:59:59 -0400

    things

---
+++ app/controllers/things_controller.rb
@@ -0,0 +1,33 @@
+class ThingsController < ApplicationController
+  def index
+    @things = Thing.all
+  end
+
+  def edit
+    @thing = Thing.find(params[:id])
+  end
+
+  def create
+    @thing = Thing.create(thing_params)
+    redirect_to :things
+  end
+
+  def update
+    @thing = Thing.find(params[:id])
+    @thing.update(thing_params)
+    redirect_to :things
+  end
+
+  def destroy
+    @thing = Thing.find(params[:id])
+    @thing.destroy
+    redirect_to :things
+  end
+
+private
+  
+  def thing_params
+    params.require(:thing).permit(:name)
+  end
+
+end
