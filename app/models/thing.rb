b8c5c850f27a8160d8fd300bb540406ac5358bcb
Adam Louis <adamlouis@gmail.com>
2015-05-08 12:54:02 -0400

    remove removed taggings on tag_list update

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -3,10 +3,11 @@ class Thing < ActiveRecord::Base
   has_many :tags, through: :taggings
 
   def tag_list=(string)
-    string.split(',').map(&:strip).reject(&:empty?).each do |tag_name|
+    taggings = string.split(',').map(&:strip).reject(&:empty?).map do |tag_name|
       tag = Tag.find_or_create_by(name: tag_name)
       Tagging.find_or_create_by(thing: self, tag: tag)
     end
+    self.taggings.where.not(id: taggings.map(&:id)).destroy_all
   end
 
   def tag_list
